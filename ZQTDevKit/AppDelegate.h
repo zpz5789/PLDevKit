86ba1b657fe9c5fa41d69721a220f68e2ec96fa0
zpz5789 <1059491105@qq.com>
2017-02-15 10:06:57 +0800

    Initial Commit

---
+++ ZQTDevKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZQTDevKit
+//
+//  Created by zpz on 2017/2/15.
+//  Copyright © 2017年 Zhiqiantong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZQTDevKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZQTDevKit
+//
+//  Created by zpz on 2017/2/15.
+//  Copyright © 2017年 Zhiqiantong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
